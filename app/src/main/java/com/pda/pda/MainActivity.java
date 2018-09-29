edc40d1db62b0420d98cd9a559ef67d53fc6d016
liangjialin <liangjialin@tbkt.cn>
2018-09-29 16:11:31 +0800

    首次提交

---
+++ app/src/main/java/com/pda/pda/MainActivity.java
@@ -0,0 +1,16 @@
+package com.pda.pda;
+
+import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.OkHttpClient;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
